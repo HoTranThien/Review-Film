f0c869dfd8b54c8d8ba9c7def5645fc6fc3b93ff
Ho Tran <htran3@strongtie.com>
2024-03-16 22:14:12 +0700

    movie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,11 +11,27 @@ export class HomeComponent implements OnInit{
   constructor(private http: HttpClient, private author:AuthorService){}
 
   movies:any;
-  
+  trending:any;
+  theatre:any;
+  viewall = false;
   ngOnInit(): void {
     this.getdata();
+    this.trending = this.theatre = this.movies;
+    this.movies.forEach((mov:any) => {
+      if (!mov.categorize.includes('trending'))
+      {
+        this.trending?.splice(mov,1);
+      }
+      if (!mov.categorize.includes('theatre'))
+      {
+        this.theatre?.splice(mov,1);
+      } 
+    });
   }
   getdata(){
     this.http.get(this.author.url).subscribe((mov)=>{this.movies = mov});
+  }
+  changeviewstate(){
+    this.viewall = !this.viewall;
   }
 }
